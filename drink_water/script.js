7a283f3bd4c979200160c9654f5eb507288fdca4
Amarta Dey <34670651+amartadey@users.noreply.github.com>
2021-01-09 14:43:40 +0530

    Drink Water

---
+++ drink_water/script.js
@@ -0,0 +1,56 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+updateBigCup();
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener("click", () => highlightCups(index));
+});
+
+function highlightCups(idx) {
+  if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+
+  if (
+    smallCups[idx].classList.contains("full") &&
+    smallCups.length - 1 == smallCups[idx]
+  ) {
+    idx--;
+  }
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalcups = smallCups.length;
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalcups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalcups) * 100}%`;
+  }
+
+  if (fullCups === totalcups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+
+    liters.innerText = `${2 - (250 * fullCups) / 1000}`;
+  }
+}
