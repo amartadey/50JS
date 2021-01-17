1e2322b40b50e81da00fc6375b922cc18c3f98d5
Amarta Dey <34670651+amartadey@users.noreply.github.com>
2021-01-17 20:06:53 +0530

    Testimonial Box Widget

--- Testimonial Box Widget/script.js
+++ Testimonial Box Widget/script.js
@@ -2,7 +2,7 @@ const testimonialsContainer = document.querySelector(".testimonials-container");
 const testimonial = document.querySelector(".testimonial");
 const userImage = document.querySelector(".user-image");
 const username = document.querySelector(".username");
-const role = document.querySelector(".roles-");
+const role = document.querySelector(".roles");
 
 const testimonials = [
   {
